ad8d49b3eae534120b81522c02fa78803abef7ef
Vladimir Alimov <v0ffk4@icloud.com>
2025-07-05 03:26:17 +0300

    chore: add tabsize

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,11 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+--  Options are automatically loaded before lazy.nvim startup
+--  Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+--  Add any additional options here
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.cmd.colorscheme "catppuccin"
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
