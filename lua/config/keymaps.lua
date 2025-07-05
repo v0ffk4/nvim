1b13633539c9c7efd70ed678892401a116bca6e4
Vladimir Alimov <v0ffk4@icloud.com>
2025-07-06 01:03:49 +0300

    chore: cl

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,7 +11,7 @@ vim.keymap.set("n", "<leader>ff", function()
 end, { desc = "Find files" })
 
 vim.keymap.set("n", "<leader>fg", function()
-  require("mini.pick").builtin.files({ tool = 'git' })
+  require("mini.pick").builtin.files({ tool = "git" })
 end, { desc = "Find git files" })
 
 vim.keymap.set("n", "<leader>fb", function()
@@ -19,25 +19,5 @@ vim.keymap.set("n", "<leader>fb", function()
 end, { desc = "Find buffers" })
 
 vim.keymap.set("n", "<leader>fr", function()
-    require("mini.pick").builtin.grep_live()
+  require("mini.pick").builtin.grep_live()
 end, { desc = "Find GREP" })
-
-
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function(event)
-    local opts = {buffer = event.buf}
-
-    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-  end,
-})
