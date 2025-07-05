3a9b5e47c0e1c794e7e3a62db873e7b350355fbf
Vladimir Alimov <v0ffk4@icloud.com>
2025-07-05 21:40:54 +0300

    chore: new beginnings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,54 +1,88 @@
 return {
-  "mason-org/mason-lspconfig.nvim",
-  opts = {
-    ensure_installed = {
-      "pyright",
-      "lua_ls",
-      "svelte",
-      "ts_ls",
-      "vscode-eslint-language-server",
-      "jsonls",
-      "html",
-      "cssls",
-      "css_variables",
-    },
-  },
-  dependencies = {
-    {
-      "mason-org/mason.nvim",
-      opts = {
-        ensure_installed = {
-          "python-lsp-server",
-          "lua-language-server",
-          "svelte-language-server",
-          "typescript-language-server",
-          "eslint-lsp",
-          "json-lsp",
-          "html-lsp",
-          "css-lsp",
-          "css-variables-language-server",
-        },
-      },
-    },
-    {
+  {
+    -- Mason
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
       "neovim/nvim-lspconfig",
-      lazy = "false",
-      config = function()
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        local lspconfig = require("lspconfig")
-        lspconfig.svelte.setup({
-          capabilities = capabilities,
-        })
-        lspconfig.html.setup({
-          capabilities = capabilities,
-        })
-        lspconfig.ts_ls.setup({
-          capabilities = capabilities,
-        })
-        lspconfig.lua_ls.setup({
-          capabilities = capabilities,
-        })
-      end,
     },
   },
+  -- Ditch
+  {
+    "hrsh7th/nvim-cmp",
+    opts = {},
+  },
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    opts = {},
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    opts = {},
+  },
+  config = function()
+    -- note: diagnostics are not exclusive to lsp servers
+    -- so these can be global keybindings
+    vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
+    vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
+    vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      desc = "LSP actions",
+      callback = function(event)
+        local opts = { buffer = event.buf }
+
+        -- these will be buffer-local keybindings
+        -- because they only work if you have an active language server
+
+        vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+        vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+        vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+        vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+        vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+        vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+        vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+        vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+        vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+        vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+      end,
+    })
+
+    local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    local default_setup = function(server)
+      require("lspconfig")[server].setup({
+        capabilities = lsp_capabilities,
+      })
+    end
+
+    require("mason").setup({})
+    require("mason-lspconfig").setup({
+      ensure_installed = {},
+      handlers = {
+        default_setup,
+      },
+    })
+
+    local cmp = require("cmp")
+
+    cmp.setup({
+      sources = {
+        { name = "nvim_lsp" },
+      },
+      mapping = cmp.mapping.preset.insert({
+        -- Enter key confirms completion item
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+
+        -- Ctrl + space triggers completion menu
+        ["<C-Space>"] = cmp.mapping.complete(),
+      }),
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+    })
+  end,
 }
