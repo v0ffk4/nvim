d65fc2549785f8d72ba6ea21ebcfa9cf0a4483f7
Vladimir Alimov <v0ffk4@icloud.com>
2025-07-05 16:21:58 +0300

    chore: install lsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,19 +1,34 @@
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-    ensure_installed = {
-      "lua-language-server",
-      "svelte-language-server",
-      "typescript-language-server",
-      "eslint-lsp",
-      "json-lsp",
-      "html-lsp",
-      "css-lsp",
-      "css-variables-language-server"
-    },
+        ensure_installed = {
+            "pyright",
+            "lua_ls",
+            "svelte",
+            "ts_ls",
+            "eslint_d",
+            "jsonls",
+            "html",
+            "css-lsp",
+            "css_variables"
+        },
     },
     dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
+        { "mason-org/mason.nvim", opts = {
+            ensure_installed = {
+                "python-lsp-server",
+                "lua-language-server",
+                "svelte-language-server",
+                "typescript-language-server",
+                "eslint_d",
+                "json-lsp",
+                "html-lsp",
+                "css-lsp",
+                "css-variables-language-server"
+            },
+        }
+    },
         "neovim/nvim-lspconfig",
     },
 }
+
