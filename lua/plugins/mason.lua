2fdb386b14015c003a260fdaafc1068fc56e1392
Vladimir Alimov <v0ffk4@icloud.com>
2025-07-05 16:46:14 +0300

    chore: formatting

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,34 +1,34 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-        ensure_installed = {
-            "pyright",
-            "lua_ls",
-            "svelte",
-            "ts_ls",
-            "eslint_d",
-            "jsonls",
-            "html",
-            "css-lsp",
-            "css_variables"
-        },
+  "mason-org/mason-lspconfig.nvim",
+  opts = {
+    ensure_installed = {
+      "pyright",
+      "lua_ls",
+      "svelte",
+      "ts_ls",
+      "eslint_d",
+      "jsonls",
+      "html",
+      "css-lsp",
+      "css_variables"
     },
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {
-            ensure_installed = {
-                "python-lsp-server",
-                "lua-language-server",
-                "svelte-language-server",
-                "typescript-language-server",
-                "eslint_d",
-                "json-lsp",
-                "html-lsp",
-                "css-lsp",
-                "css-variables-language-server"
-            },
-        }
-    },
-        "neovim/nvim-lspconfig",
-    },
+  },
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {
+      ensure_installed = {
+        "python-lsp-server",
+        "lua-language-server",
+        "svelte-language-server",
+        "typescript-language-server",
+        "eslint_d",
+        "json-lsp",
+        "html-lsp",
+        "css-lsp",
+        "css-variables-language-server"
+      },
+    }
+  },
+  "neovim/nvim-lspconfig",
+},
 }
 
