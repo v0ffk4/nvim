b11cd3b499bd5c76eeda7fc331c48eb6b9efb287
Vladimir Alimov <v0ffk4@icloud.com>
2025-07-08 12:10:54 +0300

    chore: move LSP config to lsp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,7 +9,6 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Oil
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
--- VIM
 -- Pick colortheme
 vim.keymap.set("n", "fc", function()
   local colors = vim.fn.getcompletion("", "color")
@@ -35,25 +34,3 @@ vim.keymap.set("n", "fc", function()
     },
   })
 end, { desc = "Colorscheme browser" })
-
--- LSP
-vim.api.nvim_create_autocmd("LspAttach", {
-  desc = "LSP actions",
-  callback = function(event)
-    local opts = { buffer = event.buf }
-
-    -- these will be buffer-local keybindings
-    -- because they only work if you have an active language server
-
-    vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-    vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-    vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-    vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-    vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-    vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-    vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-    vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-    vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-    vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-  end,
-})
