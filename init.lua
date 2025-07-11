ce3ee835e2d8721a8b596833b2767c9b8536a2ae
Vladimir Alimov <v0ffk4@icloud.com>
2025-07-11 09:45:02 +0300

    chore: cleanup init

--- init.lua
+++ init.lua
@@ -1,15 +1,7 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 require("config.lazy")
 require("config.options")
 require("config.keymaps")
 require("config.commands")
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.hl.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.hl.on_yank()
-  end,
-})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,11 +7,22 @@ vim.o.relativenumber = true
 vim.o.mouse = "a"
 vim.o.ignorecase = true
 vim.o.smartcase = true
+vim.o.signcolumn = "yes"
+vim.o.cursorline = true
+vim.o.scrolloff = 10
 
 vim.cmd.colorscheme("catppuccin-mocha")
-vim.cmd.set("cursorline")
 
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.softtabstop = 2
 vim.o.expandtab = false
+
+-- Highlight yanked
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
