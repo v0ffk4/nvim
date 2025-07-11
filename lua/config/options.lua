7677b53022e56222b2f44b92c1132c6a409fcb51
Vladimir Alimov <v0ffk4@icloud.com>
2025-07-11 09:20:35 +0300

    chore: add options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,17 +1,17 @@
 --  Options are automatically loaded before lazy.nvim startup
 --  Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 --  Add any additional options here
+vim.opt.fileencoding = "utf-8"
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 vim.cmd.colorscheme("catppuccin-mocha")
+vim.cmd.set("cursorline")
 
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = false
-
--- Add colorscheme picker to which-key
-require("which-key").add({
-  { "<leader>f", group = "Browse" }, -- group
-  { "<leader>fc", desc = "Colorschemes", mode = "n" },
-})
