09690a99972525d23823720557a38b9ab7aa575e
Vladimir Alimov <v0ffk4@icloud.com>
2025-07-13 22:19:28 +0300

    vshvso

--- lsp/vtsls.lua
+++ lsp/vtsls.lua
@@ -1,13 +1,35 @@
-local serverPath = vim.fn.stdpath("data")
+-- local serverPath = vim.fn.stdpath("data")
+--   .. "/mason/packages/svelte-language-server/node_modules/typescript-svelte-plugin"
+-- return {
+--   ts_ls = {
+--     globalPlugins = {
+--       {
+--         name = "typescript-svelte-plugin",
+--         -- location = { "svelte-language-server", "/node_modules/typescript-svelte-plugin" },
+--         location = serverPath,
+--         enableForWorkspaceTypeScriptVersions = true,
+--       },
+--     },
+--   },
+--   filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "svelte" },
+-- }
+
+local svelte_plugin_path = vim.fn.stdpath("data")
   .. "/mason/packages/svelte-language-server/node_modules/typescript-svelte-plugin"
+
 return {
-  ts_ls = {
-    globalPlugins = {
-      {
-        name = "typescript-svelte-plugin",
-        -- location = { "svelte-language-server", "/node_modules/typescript-svelte-plugin" },
-        location = serverPath,
-        enableForWorkspaceTypeScriptVersions = true,
+  settings = {
+    vtsls = {
+      ts_ls = {
+        globalPlugins = {
+          {
+            name = "typescript-svelte-plugin",
+            location = svelte_plugin_path,
+            enableForWorkspaceTypeScriptVersions = true,
+            languages = { "svelte" },
+            configNamespace = "typescript",
+          },
+        },
       },
     },
   },
