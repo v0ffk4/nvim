8c1e49440b6dcb79940e7560e3e25b05a2245b6a
Vladimir Alimov <v0ffk4@icloud.com>
2025-07-14 09:47:25 +0300

    chore: configure lazydev

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -4,8 +4,6 @@ return {
     ft = "lua", -- only load on lua files
     opts = {
       library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       },
     },
